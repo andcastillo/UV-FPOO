486ad50b4687723bc3688dc2d84cc260cc82de23
andres castillo <andcastillo@gmail.com>
2023-03-09 10:17:34 -0500

    chore: add comment

--- semana1/hola_mundo/main.cpp
+++ semana1/hola_mundo/main.cpp
@@ -1,3 +1,11 @@
+/*
+Para compilar escribe en la consola de replit
+g++ -o ./semana1/hola_mundo/main ./semana1/hola_mundo/*.cpp
+
+Para ejecutar el programa compilado
+./semana1/hola_mundo/main
+*/
+
 #include <iostream>
 using namespace std;
 // Suma los pares que hay entre el 0 y el 100 (excluido) 
